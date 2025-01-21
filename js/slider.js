d68d8887918af14f5f1184857b4d3564e59a57db
Tilak Raj <tilak3593.be20@chitkara.edu.in>
2025-01-21 11:35:39 +0530

    second commit

---
+++ js/slider.js
@@ -0,0 +1,32 @@
+const swiper = new Swiper('.slider-wrapper', {
+  loop: true,
+  grabCursor:true,
+  spaceBetween:30,
+
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    cliclable:true,
+    
+
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints:{
+    0:{
+      slidesPerView:1
+    },
+    620:{
+      slidesPerView:1
+    },
+    1024:{
+      slidesPerView:3
+    },
+
+  }
+});
